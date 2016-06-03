d94612886d95a44acc1851932df2e5baf9625dd3
Sheena Takkenberg <sheena.takkenberg@gmail.com>
2016-06-03 12:36:36 +1000

    added login logout etc to the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  resources :homepage, only: :index
+
+  root 'dashboard#index'
+
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
